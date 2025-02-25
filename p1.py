058ff10ee3ecd8f438dc0a4de99394fa8dd6e2e4
chundi-vishnu <chundivishnu28@gmail.com>
2025-02-25 16:03:06 +0530

    intial commit

--- p1.py
+++ p1.py
@@ -1,1 +1,14 @@
 #nearst number to 0
+def closest_to_zero(lis):
+    if not lis:
+        return None  
+    
+    closest = lis[0]  
+
+    for num in lis:
+        if abs(num) < abs(closest) or (abs(num) == abs(closest) and num > closest):
+            closest = num
+    
+    return closest
+numbers = [-10, 3, 5, -2, 2, -1, 1]
+print("Closest to zero:", closest_to_zero(numbers))
---
+++ p2.py
@@ -0,0 +1,12 @@
+a=9
+b=10
+# a,b=b,a
+print("before swapping:")
+print(a)
+print(b)
+a=a+b
+b=a-b
+a=a-b
+print("after swapping:")
+print(a)
+print(b)
